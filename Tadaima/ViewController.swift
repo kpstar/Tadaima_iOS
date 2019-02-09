b17454f40fb8757e86c9c6a7fb692e85d9d04ce9
KpStar <kpstar.mc@gmail.com>
2019-02-09 10:29:48 +0200

    Initial Commit

---
+++ Tadaima/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tadaima
+//
+//  Created by KpStar on 2/9/19.
+//  Copyright © 2019 Tadaima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
